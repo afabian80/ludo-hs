e1bdb4d42e3a4ab41853c6c0f1b3110d4ab655ed
Akos Fabian <fabian.akos@gmail.com>
2016-05-14 09:27:33 +0200

    refactor

--- main.hs
+++ main.hs
@@ -1,8 +1,15 @@
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# ANN module "HLint: ignore Use concatMap" #-}
 
 data Player = Red | Green | Blue | Yellow deriving (Show, Eq, Enum)
 type Position = Int
 data Piece = Piece Player Position deriving (Show)
+
+getPlayer :: Piece -> Player
+getPlayer (Piece player _) = player
+
+getPosition :: Piece -> Position
+getPosition (Piece _ position) = position
 
 randomNumbers :: [Int]
 randomNumbers = [1, 6, 5, 1, 1, 1, 5, 1, 6, 5, 2, 2, 2, 5, 2, 5, 4, 4, 2, 1, 4, 3, 5, 4, 6, 2, 2, 2, 2, 4, 4, 5, 6, 1, 5, 2, 6, 1, 6, 5, 2, 5, 6, 6, 5, 6, 2, 2, 6, 2, 3, 2, 2, 1, 1, 6, 5, 5, 1, 6, 4, 6, 6, 5, 6, 5, 2, 2, 4, 6, 3, 3, 5, 6, 3, 4, 1, 4, 1, 1, 6, 3, 3, 1, 6, 1, 4, 2, 4, 5, 6, 2, 4, 5, 6, 6, 4, 3, 4, 6]
@@ -14,24 +21,18 @@ main :: IO ()
 main = do
         print pieces
         let player = Red
-        let (dice, nextPlayer, randoms) = roll player randomNumbers
-        --let pieceToMove = chooseBestPieceFor dice nextPlayer pieces
+        let (number, nextPlayer, randoms) = roll player randomNumbers
+        --let pieceToMove = chooseBestPieceFor number nextPlayer pieces
         putStrLn "done"
 
 roll :: Player -> [Int] -> (Int, Player, [Int])
-roll player randoms = (dice, nextPlayerFor dice player, tail randoms)
-        where dice = head randoms
+roll player randoms = (number, nextPlayerFor number player, tail randoms)
+        where number = head randoms
 
 nextPlayerFor :: Int -> Player -> Player
 nextPlayerFor 6 player = player
 nextPlayerFor _ Yellow = Red
 nextPlayerFor _ player = succ player
-
-getPlayer :: Piece -> Player
-getPlayer (Piece player _) = player
-
-getPosition :: Piece -> Position
-getPosition (Piece _ position) = position
 
 moveSingle :: Piece -> Int -> Piece
 moveSingle (Piece player position) step = Piece player (position + step)
